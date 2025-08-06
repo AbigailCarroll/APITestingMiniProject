ec93b65de2d907705611c09f9e979559726b2bf4
Abdallah Alhajahmed <2419103A@student.gla.ac.uk>
2025-08-06 12:25:26 +0100

    Save current work before switching branches

--- src/test/java/utils/Config.java
+++ src/test/java/utils/Config.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-
-
     public class Config {
         private static final Properties properties = new Properties();
 
