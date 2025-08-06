2f21dc6ed8856c3d7daf8827724cca1e1927ccf1
franc <francisadewale1992@gmail.com>
2025-08-06 11:51:01 +0100

    test passes

--- src/test/java/utils/API.java
+++ src/test/java/utils/API.java
@@ -1,4 +1,28 @@
 package utils;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Map;
+
 public class API {
+
+    private static final String BASE_URI = "https://automationexercise.com";
+    private static final String POST_PRODUCTS_LIST_PATH = "api/productsList";
+
+    public static RequestSpecification postProductsListRequestSpec() {
+        return getBaseSpecBuilder()
+                .build();
+    }
+
+    private static RequestSpecBuilder getBaseSpecBuilder() {
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(API.POST_PRODUCTS_LIST_PATH)
+                .addHeaders(Map.of(
+                        "Accept", "*/*"
+                ));
+    }
+
+
 }
