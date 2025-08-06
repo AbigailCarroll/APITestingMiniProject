9114f7dbfc29bf9d1ea3de917515b17fe41c87b2
franc <francisadewale1992@gmail.com>
2025-08-06 14:49:45 +0100

    refactor stuff

---
+++ src/test/java/pojos/ProductList/PostProductListPOJO.java
@@ -0,0 +1,14 @@
+package pojos.ProductList;
+
+public class PostProductListPOJO {
+	private String message;
+	private int responseCode;
+
+	public String getMessage(){
+		return message;
+	}
+
+	public int getResponseCode(){
+		return responseCode;
+	}
+}
--- src/test/java/restassured/PostProductList.java
+++ src/test/java/restassured/PostProductList.java
@@ -1,29 +1,37 @@
 package restassured;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojos.ProductList.PostProductListPOJO;
 import utils.API;
 
 public class PostProductList {
     private static Response response;
-
+    static PostProductListPOJO postProductListPOJO;
 
     @BeforeAll
     public static void setup() {
 
+        RestAssured.registerParser("text/html", Parser.JSON);
+
+
         response = RestAssured
                 .given()
-                .spec(API.postProductListSpec())
+                    .spec(API.postProductListSpec())
                 .when()
-                .post()
+                    .post()
                 .then()
                 .log().all()
                 .extract().response();
+
+        postProductListPOJO = response.as(PostProductListPOJO.class);
 
     }
 
@@ -31,7 +39,7 @@ public class PostProductList {
     @Test
     public void postRequestReturn405StatusCode() {
 
-        MatcherAssert.assertThat(response.jsonPath().getString("responseCode"), Matchers.is("405"));
+        MatcherAssert.assertThat(postProductListPOJO.getResponseCode(), Matchers.is(405));
 
 
     }
